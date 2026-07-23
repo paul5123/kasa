b95416cb3df6145040981ab38515106d162578d1
Paul5123 <paul.guillon17@orange.fr>
2026-07-24 01:46:01 +0200

    Ajout de la page a propos et des collapse

---
+++ src/components/Collapse/index.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './Collapse.scss'
+import arrow from '../../assets/arrow.png'
+
+function Collapse({title, content }) {
+    const [isOpen, setIsOpen] = useState(false)
+  return (
+    <div className="collapse">
+        <div className="collapse__title"
+        onClick={() => setIsOpen(!isOpen)}
+        >
+            <h2>{title}</h2>
+            <img
+                src={arrow}
+                alt=""
+                 className={isOpen ? "collapse__arrow open" : "collapse__arrow"}
+            />
+        </div>
+        {isOpen && ( 
+        <div className="collapse__content">
+            <p>{content}</p>
+        </div>
+        )}
+    </div>
+  )
+}
+
+export default Collapse

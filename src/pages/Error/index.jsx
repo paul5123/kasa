1d0445385ebfc90ead25c3bfb74abd517f332de0
Paul5123 <paul.guillon17@orange.fr>
2026-07-24 13:13:57 +0200

    La page erreur

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,5 +1,20 @@
+import { Link } from 'react-router-dom'
+import './Error.scss'
+
 function Error() {
-  return <h1>Erreur 404</h1>
+  return (
+    <section className="error">
+      <h1 className="error__code">404</h1>
+
+      <p className="error__message">
+        Oups ! La page que vous demandez n'existe pas.
+      </p>
+
+      <Link className="error__link" to="/">
+        Retourner sur la page d’accueil
+      </Link>
+    </section>
+  )
 }
 
 export default Error
